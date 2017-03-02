988b223b9cf69afa743279d175b3384b74be1e8d
jmichel3 <jon.michelson93@gmail.com>
2017-03-02 00:02:47 -0500

    major cleaning for repo size reduction

--- code/matlab_toolboxes/MIR/MIRToolbox/@mirdata/display.m
+++
@@ -1,9 +0,0 @@
-function display(d)
-% MIRDATA/DISPLAY display of a MIR data
-
-ST = dbstack;
-if strcmp(ST(end).file,'arrayviewfunc.m')
-    mirdisplay(d,inputname(1),'nofigure');
-else
-    mirdisplay(d,inputname(1));
-end
--- code/matlab_toolboxes/MIR/MIRToolbox/@mirmidi/mirsave.m
+++
@@ -1,7 +0,0 @@
-function mirsave(m)
-
-nmat = get(m,'Data');
-n = get(m,'Name');
-for k = 1:length(nmat)
-    writemidi(nmat{k},[n{k},'.mid'],120,60);
-end
