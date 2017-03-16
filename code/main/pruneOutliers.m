cbf14f26cec0ab862a3e05428c3ed520f1b02983
jmichel3 <jon.michelson93@gmail.com>
2017-03-15 23:12:44 -0400

    outlier removal fn; proposal mat updates

---
+++ code/main/pruneOutliers.m
@@ -0,0 +1,45 @@
+function PRUNED = pruneOutliers(FEAT, METHOD, varargin)
+% PRUNED = PRUNEOUTLIERS(FEAT, METHOD, [ NUMDEVS]) 
+% Finds outliers, if any, in FEAT and prunes them from the feature
+% set. Pruned feature PRUNED is returned. PRUNEOUTLIERS() operates only on
+% vectors since matrix columns may contain variable numbers of
+% outliers and dimensions may not be preserved.
+%
+% METHOD options
+% 'mode': median absolute deviations
+% 'mean': mean absolute deviations
+%
+% NUMDEVS options
+% Integer specifying multiples of absolute deviations that qualify an outlier.
+% Defaults to 3 if unspecified.
+
+if size(FEAT,2) ~= 1
+   error('pruneOutliers must take in a column vector'); 
+end
+
+if ~isempty(varargin)
+    MULT = varargin{1};
+else MULT = 3;
+end
+
+if isequal(METHOD,'median')
+    MEASURE = median(FEAT)
+else if isequal(METHOD, 'mean')
+    MEASURE = mean(FEAT)
+    else error('Invalid pruneOutliers METHOD');
+    end
+end
+
+M = length(FEAT);
+PRUNED = FEAT;
+
+% Calculate threshold above which a difference with outlier will be positive
+THRESHOLD = abs(MULT * MEASURE)
+RECTFEAT = abs(FEAT); % Rectified features
+PRUNED(RECTFEAT > THRESHOLD) = 0;
+
+% Substitute outliers in FEAT with MEASURE values as to not affect
+% post-prune statistics nor dimension
+PRUNED(RECTFEAT > THRESHOLD) = MEASURE;
+
+end
